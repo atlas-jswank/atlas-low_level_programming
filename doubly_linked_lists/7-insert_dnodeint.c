f8602762120b6e45c6dd8eb042f86166d2982e3b
Jeremiah Swank <jeremiah.swank@atlasschool.com>
2024-10-15 13:44:50 -0500

    Task 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -14,44 +14,30 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i = 0;
 	dlistint_t *current = *h;
-	dlistint_t *new = malloc(sizeof(dlistint_t));
-
-	if (new == NULL)
-		return (NULL);
-
-	new->n = n;
-
-	if (*h == NULL)
-	{
-		*h = new;
-		return (new);
-	}
-
-	if (idx == 0)
-	{
-		new->next = current;
-		*h = new;
-
-		current->prev = new;
-		return (new);
-	}
-
-	while (current != NULL && i < idx - 1)
-	{
-		current = current->next;
-		i++;
-	}
+	dlistint_t *new;
 
 	if(current == NULL)
 		return (NULL);
 
+	if(idx == 0)
+		return (add_dnodeint(h, n));
+	
+	for(i = 0; current != NULL && i < idx-1; i++)
+		current = current->next;
 
-	new->next = current->next;
+	if(current->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
 	new->prev = current;
+	new->next = current->next;
 
-	if (current->next != NULL)
-		current->next->prev = new;
+	current->next->prev = new;
 	current->next = new;
-
+	
 	return (new);
 }
