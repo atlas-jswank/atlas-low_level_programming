406ff5709decac6f2df3f5ef1b9c137434209e60
Jeremiah Swank <jeremiah.swank@atlasschool.com>
2024-10-15 09:04:29 -0500

    doubly_linked_lists: task 2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -12,14 +12,15 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new = malloc(sizeof(dlistint_t));
+
 	if (new == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	new->n = n;
 	new->prev = NULL;
 	new->next = *head;
-	if(*head != NULL)
+	if (*head != NULL)
 		(*head)->prev = new;
 	*head = new;
 	return (new);
