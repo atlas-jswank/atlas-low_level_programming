a8081fdaac4c7b57699ffa5b74bc86932ba16d04
Jeremiah Swank <jeremiah.swank@atlasschool.com>
2024-10-15 08:42:08 -0500

    doubly_linked_lists: task 1

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -4,19 +4,19 @@
 #include "lists.h"
 
 /**
- * dlistint_len - Prints all the elements of a dlistint_t list.
- * @head: The head of the dlistint_t list.
- * Return: The number of nodes in the list.
- */
+	* dlistint_len - Prints all the elements of a dlistint_t list.
+	* @head: The head of the dlistint_t list.
+	* Return: The number of nodes in the list.
+	*/
 size_t dlistint_len(dlistint_t *h)
 {
-    dlistint_t *current = h;
-    size_t length = 0;
+				dlistint_t *current = h;
+				size_t length = 0;
 
-    while (current != NULL)
-    {
-        current = current->next;
-        length++;
-    }
-    return (length);
+				while (current != NULL)
+				{
+								current = current->next;
+								length++;
+				}
+				return (length);
 }
