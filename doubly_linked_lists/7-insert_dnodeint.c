8b9fd258665ef50ea8c946910b5d6ecf482ff0ac
Jeremiah Swank <jeremiah.swank@atlasschool.com>
2024-10-15 11:25:14 -0500

    task 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -21,6 +21,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	new->n = n;
 
+	if(h == NULL){
+		return (new);
+	}
+
 	if(idx == 0){
 		new->next = current;
 		*h = new;
