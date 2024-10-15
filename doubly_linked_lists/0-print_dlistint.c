f317086307ba076faca01d1e1c889146a9a36d1b
Jeremiah Swank <jeremiah.swank@atlasschool.com>
2024-10-15 08:22:15 -0500

    doubly_linked_lists: task 0

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+size_t print_dlistint(dlistint_t *head){
+
+   dlistint_t *current = head;
+   size_t length = 0;
+   while(current != NULL){
+      printf("%d\n", current->n);
+      current = current->next;
+      length++;
+   }
+   
+   return length;
+}
