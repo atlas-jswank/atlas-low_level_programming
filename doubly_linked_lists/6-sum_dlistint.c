8d5c146ee819ad94555ef8ce29ee86b8e9fa551b
Jeremiah Swank <jeremiah.swank@atlasschool.com>
2024-10-15 10:32:19 -0500

    doubly_linked_lists task 6

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * sum_dlistint - sums teh values in a dlistint_t linked list.
+ * @head: The head of the dlistint_t list.
+ * Return: nth node
+ */
+int sum_dlistint(dlistint_t *head)
+{
+    unsigned int sum = 0;
+	dlistint_t *current = head;
+
+	while (current != NULL)
+	{
+        sum += current->n;
+		current = current->next;
+	}
+	return (sum);
+}
