b7e778869d1456a896ac71476867ac69324ddcd1
Jeremiah Swank <jeremiah.swank@atlasschool.com>
2024-10-15 14:47:09 -0500

    Task 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -13,30 +13,30 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int i = 0;
 	dlistint_t *current = *head;
-    dlistint_t *prev, *next;
+	dlistint_t *prev, *next;
 
-     if(*head == NULL)
-        return (-1);
+	if(*head == NULL)
+		return (-1);
 
 	for (i = 0; current != NULL && i < index; i++)
 		current = current->next;
 
-    if(current == NULL)
-        return (-1);
+	if(current == NULL)
+		return (-1);
 
-    prev = current->prev;
-    next = current->next;
+	prev = current->prev;
+	next = current->next;
 
-    if(index == 0)
-        *head = next;
+	if(index == 0)
+		*head = next;
 
-    if(prev != NULL)
-        prev->next = next;
-    
-    if(next != NULL)
-        next->prev = prev;
-    
-    free(current);
+	if(prev != NULL)
+		prev->next = next;
 
-    return (0);
+	if(next != NULL)
+		next->prev = prev;
+
+	free(current);
+
+	return (0);
 }
