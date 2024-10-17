62b7b8033d7af2d2f894321595c0fbc414f42bc0
Jeremiah Swank <jeremiah.swank@atlasschool.com>
2024-10-17 14:42:04 -0500

    hash table task 3

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,21 @@
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+    unsigned long int index;
+    hash_node_t *node;
+
+    if(ht == NULL || key == NULL || *key == '\0' || value == NULL)
+        return 0;
+
+    index = key_index((unsigned char *)key, ht->size);
+
+    node = ht->array[index];
+    while(node != NULL)
+    {    
+        if( strcmp(node->key, key) == 0)
+        {
+            return node->value;
+        }
+        node = node->next;
+    }
+    return NULL;
+}
