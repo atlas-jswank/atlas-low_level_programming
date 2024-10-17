ceeee9277a98ee265789c8c4163b73ff2825ea89
Jeremiah Swank <jeremiah.swank@atlasschool.com>
2024-10-17 14:38:58 -0500

    hash table task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -5,14 +5,13 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index, i = 0;
+    unsigned long int index;
     hash_node_t *node;
 
     if(ht == NULL || key == NULL || *key == '\0' || value == NULL)
         return 0;
 
     index = key_index((unsigned char *)key, ht->size);
-    printf("%lu\n", index);
 
     node = ht->array[index];
     while(node != NULL)
